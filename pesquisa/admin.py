e31dd54ab49d9020c9d99ff51a638b35b2610ea9
Luan <Luan.staner@gmail.com>
2019-02-27 22:10:16 -0300

    Iniciando aplicação de banco no html

--- pesquisa/admin.py
+++ pesquisa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Alternativa, Pergunta
+admin.site.register(Pergunta)
+admin.site.register(Alternativa)
+
+# Passar para o admin do django as classe para add no bd
--- pesquisa/models.py
+++ pesquisa/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 class Pergunta(models.Model):
+
+    def __str__(self):
+        return self.texto
+
     texto = models.CharField(max_length=300)
     data = models.DateTimeField(default = timezone.now)
 
 class Alternativa(models.Model):
+
+    def __str__(self):
+        return self.texto
+
     pergunta = models.ForeignKey(Pergunta, on_delete = models.CASCADE)
     texto = models.CharField(max_length=150)
     votos = models.IntegerField(default=0)
+
