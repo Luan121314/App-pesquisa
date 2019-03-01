cc4263ccd8026edff6f7f206736b18ceeffe5189
Luan <Luan.staner@gmail.com>
2019-03-01 20:44:40 -0300

    Setando publicação no heroku

--- opniao/urls.py
+++ opniao/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('pesquisa/', include('pesquisa.urls')),
     path('admin/', admin.site.urls),
+    
 ]
--- pesquisa/views.py
+++ pesquisa/views.py
@@ -5,3 +5,18 @@ def index(request):
     todas = Pergunta.objects.all()
     return render(request, 'index.html',{"perguntas": todas
     })
+def resposta(request, num_pergunta):
+    pergunta = Pergunta.objects.get(pk=num_pergunta)
+    return render(request, 'responder.html',{"pergunta": pergunta})
+
+def votar(request):
+    escolhida = Alternativa.objects.get(pk=voto)
+    escolhida.votos +=1
+    escolhida.save()
+    voto =request.POST['escolha']
+    return render(request, 'votar.html',{"numero": voto
+    })
+
+def resultados(request,num_pergunta):
+    pergunta = Pergunta.objects.get(pk)
+    return render(request, 'resultados.html')
